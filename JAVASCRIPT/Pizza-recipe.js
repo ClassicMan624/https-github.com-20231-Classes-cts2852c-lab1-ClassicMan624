9703e7e0d751f38eb2a590ee19aa48cddc30c1c1
ClassicMan624 <99371527+ClassicMan624@users.noreply.github.com>
2022-09-06 11:31:49 -0400

    Initial Commit

---
+++ JAVASCRIPT/Pizza-recipe.js
@@ -0,0 +1,9 @@
+
+    
+function pizzaIngredients() {
+        document.write("FOR PIZZA THE INGREDIENTS YOU WILL NEED ARE!\n\n - Flat pizza bread!\n\n - Sauce of choice(Tomato sauce is my favorite!)\n\n - Cheese!!\n\n - Choice of Toppings (I Like Chicken and onions on mine :D!)\n");
+    }
+function pizzaInstructions() {
+    document.write(" 1. Make sure all ingredients are together and available!\n\n 2. Place flat pizza bread on a sanitary place.\n\n 3. Spread in circular motion the choice of sauce.\n\n 4. Once sauce is spread out evenly, add cheese!!! (as much as possible >:).\n\n 5. Now add toppings if chosen to add some!\n\n 6. Set oven to 350 degrees to preheat.\n\n 7. Once pizza is how you desire it and oven has completed preheating, place in oven.\n\n 8.Set timer for 30 minutes.\n\n 9. Once pizza finishes take it out and cut into 8 pieces.\n\n 10. ENJOY! ")
+}
+
